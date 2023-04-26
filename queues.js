e510e856108d5c6a3f4e3528a44190d40a09af1d
jonathan2025 <Jonathan.Liang75@outlook.com>
2023-04-25 23:05:50 -0400

    finished the solution to the exercise

--- queues.js
+++ queues.js
@@ -5,16 +5,21 @@
 const Queue = require('./Queue');
 
 const load = flights => {
-
+  const runway = new Queue(3)
   flights.forEach(flight => {
-  
+    try{
+      runway.enqueue(flight)
+    console.log(`${flight} taxi to runway.`);
+    } catch(error){
+      console.log('Runway full!');
+    }
   });
-    
+  return runway
 };
 
 const clear = runway => {
-  while(false) {
-    
+  while(runway.isEmpty() === false) {
+    const cleared = runway.dequeue()
     console.log('\nFlights wait...\n');
     console.log(`${cleared}, is cleared for takeoff!\n${cleared} in air.`);
   }
